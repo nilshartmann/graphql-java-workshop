d698f9709a2e20ff1561c4d5a154672bb128234c
Nils Hartmann <nils@nilshartmann.net>
2019-08-20 10:37:37 +0200

    rename

--- app/userservice/src/main/java/nh/graphql/userservice/UserServiceApplication.java
+++ app/userservice/src/main/java/nh/graphql/userservice/UserServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserserviceApplication {
+public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserserviceApplication.class, args);
+		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
 }
