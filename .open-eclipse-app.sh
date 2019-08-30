7fc379151962ee6a1efa71cbbf94c5499f831b12
Nils Hartmann <nils@nilshartmann.net>
2019-08-30 17:51:08 +0200

    setup

---
+++ .open-eclipse-app.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+cd /Applications
+open -n SpringToolSuite4.app/ --args -data ~/develop/javascript/graphql-java-workshop/.sts-workspace-app/
--- .open-workspace-eclipse.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-cd /Applications
-open -n SpringToolSuite4.app/ --args -data ~/develop/javascript/graphql-java-workshop/.sts-workspace-workspace/
