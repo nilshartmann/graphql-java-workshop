ac3021850399e4e9f3127be9f4609ed6907980a6
Nils Hartmann <nils@nilshartmann.net>
2019-09-01 14:42:45 +0200

    uebungen

--- code/99_fertig/src/main/resources/public/playground/download-playground.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-curl -O https://cdn.jsdelivr.net/npm/graphql-playground-react/build/static/css/index.css
-curl -O https://cdn.jsdelivr.net/npm/graphql-playground-react/build/favicon.png
-curl -O https://cdn.jsdelivr.net/npm/graphql-playground-react/build/static/js/middleware.js
