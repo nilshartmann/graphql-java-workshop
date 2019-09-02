9eafcf3091c47c2439b017bc360fef46f4218467
Nils Hartmann <nils@nilshartmann.net>
2019-09-02 21:39:12 +0200

    config for idea

--- app/frontend/apollo.config.js
+++ app/frontend/apollo.config.js
@@ -3,7 +3,7 @@ module.exports = {
     includes: ["src/**/*.{ts,tsx}"],
     service: {
       name: "project-example",
-      url: "http://localhost:4000/"
+      url: "http://localhost:5000/graphql"
     }
   }
 };
