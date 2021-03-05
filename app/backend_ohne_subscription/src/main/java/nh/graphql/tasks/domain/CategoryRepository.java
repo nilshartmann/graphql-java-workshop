551f31699b7fbc774ff763cd9e2fb6fcc2d818f6
Nils Hartmann <nils@nilshartmann.net>
2021-03-05 11:58:12 +0100

    backend_one_subscription weg

--- app/backend_ohne_subscription/src/main/java/nh/graphql/tasks/domain/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package nh.graphql.tasks.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-/**
- * @author Nils Hartmann (nils@nilshartmann.net)
- */
-public interface CategoryRepository extends CrudRepository<Category, Long> {
-}
--- app/backend_ohne_subscription/src/main/java/nh/graphql/tasks/domain/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package nh.graphql.tasks.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-/**
- * @author Nils Hartmann (nils@nilshartmann.net)
- */
-public interface TaskRepository extends CrudRepository<Task, Long> {
-}
